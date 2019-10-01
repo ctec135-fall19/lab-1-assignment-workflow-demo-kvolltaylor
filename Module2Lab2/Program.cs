a9ec16225c34b490534c5cbba8af3fa1747da488
Katrina <k.voll-taylor@students.clark.edu>
2019-10-01 14:42:23 -0700

    Lab 2 submission

---
+++ Module2Lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my baby, hello my honey, hello my ragtime gal!");
+        }
+    }
+}
